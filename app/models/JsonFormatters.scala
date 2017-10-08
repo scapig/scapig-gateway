6c4ec83c565164d0edea689ba3fbeb11a20dc172
tomvdv59 <t.vandevelde@yahoo.fr>
2017-10-08 17:57:44 +0100

    fix compile error

---
+++ app/models/JsonFormatters.scala
@@ -0,0 +1,10 @@
+package models
+
+import play.api.libs.json.{JsValue, Json, Writes}
+
+object JsonFormatters {
+
+  implicit val errorResponseWrites = new Writes[ErrorResponse] {
+    def writes(e: ErrorResponse): JsValue = Json.obj("code" -> e.errorCode, "message" -> e.message)
+  }
+}
